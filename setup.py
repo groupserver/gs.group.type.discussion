b355ce0296a736d3d5ef82bfc40f258473166b81
Michael JasonSmith <mpj17@onlinegroups.net>
2014-07-17 13:10:48 -0700

    PEP-8 OCD

--- setup.py
+++ setup.py
@@ -1,50 +1,67 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
-setup(name='gs.group.type.discussion',
-    version=version,
-    description="Support for discussion groups in GroupServer",
-    long_description=open("README.txt").read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.txt")).read(),
-    classifiers=[
-      "Development Status :: 4 - Beta",
-      "Environment :: Web Environment",
-      "Framework :: Zope2",
-      "Intended Audience :: Developers",
-      "License :: Other/Proprietary License",
-      "Natural Language :: English",
-      "Operating System :: POSIX :: Linux"
-      "Programming Language :: Python",
-      "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    keywords='user, group, page',
-    author='Michael JasonSmith',
-    author_email='mpj17@onlinegroups.net',
-    url='http://groupserver.org',
-    license='ZPL',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.type',],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'setuptools',
-        'zope.app.apidoc',
-        'zope.cachedescriptors',
-        'zope.component',
-        'gs.group.base',
-        'gs.group.member.base',
-        'gs.group.member.canpost',
-        'gs.group.privacy',
-        'gs.profile.email.base',
-        'Products.GSSearch',
-        'Products.XWFCore',
-    ],
-    entry_points="""
-    # -*- Entry points: -*-
-    """,
-)
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"),
+                 encoding='utf-8') as f:
+    long_description += '\n' + f.read()
 
+setup(name='gs.group.type.discussion',
+      version=version,
+      description="Support for discussion groups in GroupServer",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "Intended Audience :: Developers",
+          'License :: OSI Approved :: Zope Public License',
+          "Natural Language :: English",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='user, group, discussion',
+      author='Michael JasonSmith',
+      author_email='mpj17@onlinegroups.net',
+      url='https://source.iopen.net/groupserver/gs.group.type.discussion/',
+      license='ZPL 2.1',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['gs', 'gs.group', 'gs.group.type', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.app.apidoc',
+          'zope.cachedescriptors',
+          'zope.component',
+          'gs.group.base',
+          'gs.group.member.base',
+          'gs.group.member.canpost',
+          'gs.group.privacy',
+          'gs.profile.email.base',
+          'Products.GSSearch',
+          'Products.XWFCore',
+      ],
+      entry_points="""
+          # -*- Entry points: -*-
+      """,)
